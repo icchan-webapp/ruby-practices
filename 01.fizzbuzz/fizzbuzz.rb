3d6d4a5dc015ecd0bbb4a34defa9427f8f141489
icchan-webapp <ikkun.official@gmail.com>
2023-05-08 11:34:25 +0900

    条件部分のソースコードが短くなるよう変更。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -4,7 +4,7 @@ NUMBER_OF_DISPLAY_TIMES = 20
 
 for num in 1 .. NUMBER_OF_DISPLAY_TIMES do
   result =
-    if num % 3 == 0 && num % 5 == 0
+    if num % 15 == 0
       "FizzBuzz"
     elsif num % 3 == 0
       "Fizz"
