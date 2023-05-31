a138035dd197caf1b24e5a93b168779d150a7159
icchankun <ikkun.official@gmail.com>
2023-05-31 11:33:40 +0900

    nextを使用し、記述コードを短縮化。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -19,15 +19,16 @@ frames = shots.each_slice(2).to_a
 
 point = 0
 frames.each_with_index do |frame, i|
-  if i > 8
-    point += frame.sum
-  elsif frame[0] == 10 # strike
-    point += 10 + frames[i + 1].sum
+  point += frame.sum
+
+  # 10フレーム目である場合、またはストライク・スペアでない場合は、次の繰り返しにジャンプする。
+  next if i > 8 || frame.sum != 10
+
+  if frame[0] == 10 # ストライクの場合
+    point += frames[i + 1].sum
     point += frames[i + 2][0] if frames[i + 1][0] == 10
-  elsif frame.sum == 10 # spare
-    point += 10 + frames[i + 1][0]
-  else
-    point += frame.sum
+  else # スペアの場合
+    point += frames[i + 1][0]
   end
 end
 puts point
