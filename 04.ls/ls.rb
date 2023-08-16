ef6e5e066276fb70e22efbbd5ed42cf56ee859d2
icchankun <ikkun.official@gmail.com>
2023-08-16 11:28:28 +0900

    「indent_of_file」を「index_of_file」に変更。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -10,11 +10,11 @@ row = (files.size.to_f / LINE).ceil
 
 (0..row - 1).each do |i|
   files_for_row = []
-  indent_of_file = i
+  index_of_file = i
 
-  while indent_of_file <= files.size - 1
-    files_for_row << files[indent_of_file].ljust(indent)
-    indent_of_file += row
+  while index_of_file <= files.size - 1
+    files_for_row << files[index_of_file].ljust(indent)
+    index_of_file += row
   end
 
   puts files_for_row.join
