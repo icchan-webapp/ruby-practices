3768e92139364c100384c545a34205ec6ac3fa73
icchankun <ikkun.official@gmail.com>
2023-09-04 14:08:22 +0900

    -lオプションの有無で処理を変更する箇所を見やすくするため、処理内容をメソッド化。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -7,90 +7,106 @@ require 'optparse'
 require 'etc'
 
 params = ARGV.getopts('l').transform_keys(&:to_sym)
-
 files = Dir.glob('*')
 
-if params[:l]
-  def mode(file_status)
-    mode = file_status.mode.to_s(8)
-    mode.prepend('0') if mode.size != 6
-    mode
-  end
+NUMBER_OF_COLUMNS = 3
 
-  PERMISSION_MARKS = { r: 4, w: 2, x: 1 }
+def execute_ls(files)
+  indent = files.map(&:size).max + 1
+  number_of_rows = (files.size.to_f / NUMBER_OF_COLUMNS).ceil
 
-  def change_numbers_to_chars(index, number, special_permission)
-    PERMISSION_MARKS.map do |permission_mark, value|
-      if index == 2 && permission_mark == :x && special_permission != '0'
-        change_x_to_special_permission(number, special_permission)
-      elsif number >= value
-        number -= value
-        permission_mark
-      else
-        '-'
-      end
+  (0..number_of_rows - 1).each do |i|
+    files_for_row = []
+    index_of_file = i
+
+    while index_of_file <= files.size - 1
+      files_for_row << files[index_of_file].ljust(indent)
+      index_of_file += number_of_rows
     end
+
+    puts files_for_row.join
   end
+end
 
-  SPECIAL_PERMISSIONS = { t: '1', sgid: '2', suid: '4' }
+def mode(file_status)
+  mode = file_status.mode.to_s(8)
+  mode.prepend('0') if mode.size != 6
+  mode
+end
 
-  def change_x_to_special_permission(number, special_permission)
-    special_permission_key = SPECIAL_PERMISSIONS.key(special_permission)
-    number < 1 ? special_permission_key.upcase[0] : special_permission_key[0]
-  end
+PERMISSION_MARKS = { r: 4, w: 2, x: 1 }
 
-  def file_type_chr(file)
-    file_type = File.ftype(file)
-
-    case file_type
-    when 'fifo'
-      'p'
-    when 'file'
+def change_numbers_to_chars(index, number, special_permission)
+  PERMISSION_MARKS.map do |permission_mark, value|
+    if index == 2 && permission_mark == :x && special_permission != '0'
+      change_x_to_special_permission(number, special_permission)
+    elsif number >= value
+      number -= value
+      permission_mark
+    else
       '-'
-    else
-      file_type.chr
     end
   end
+end
 
-  blocks_total = 0
+SPECIAL_PERMISSIONS = { t: '1', sgid: '2', suid: '4' }
 
-  files_with_details =
-    files.map do |file|
-      file_status = File.stat(file)
-      mode = mode(file_status)
-      permission_numbers = mode[3, 3]
-      special_permission = mode[2]
+def change_x_to_special_permission(number, special_permission)
+  special_permission_key = SPECIAL_PERMISSIONS.key(special_permission)
+  number < 1 ? special_permission_key.upcase[0] : special_permission_key[0]
+end
 
-      permissions =
-        permission_numbers.each_char.map.with_index do |permission_number, index|
-          number = permission_number.to_i
-          change_numbers_to_chars(index, number, special_permission)
-        end
+def file_type_chr(file)
+  file_type = File.ftype(file)
 
-      nlink = file_status.nlink
-      uid = Etc.getpwuid(file_status.uid).name
-      gid = Etc.getgrgid(file_status.gid).name
-      size = file_status.size
-      mtime = file_status.mtime.strftime('%-m %-d %H:%M')
-      blocks_total += file_status.blocks
+  case file_type
+  when 'fifo'
+    'p'
+  when 'file'
+    '-'
+  else
+    file_type.chr
+  end
+end
 
-      [permissions.unshift(file_type_chr(file)).join, nlink, uid, gid, size, mtime, file]
-    end
+files_with_details =
+  files.map do |file|
+    file_status = File.stat(file)
+    mode = mode(file_status)
+    permission_numbers = mode[3, 3]
+    special_permission = mode[2]
 
-  def margin(index)
-    if index.zero?
-      0
-    elsif [1, 2, 6].include?(index)
-      1
-    else
-      2
-    end
+    permissions =
+      permission_numbers.each_char.map.with_index do |permission_number, index|
+        number = permission_number.to_i
+        change_numbers_to_chars(index, number, special_permission)
+      end
+
+    nlink = file_status.nlink
+    uid = Etc.getpwuid(file_status.uid).name
+    gid = Etc.getgrgid(file_status.gid).name
+    size = file_status.size
+    mtime = file_status.mtime.strftime('%-m %-d %H:%M')
+
+    [permissions.unshift(file_type_chr(file)).join, nlink, uid, gid, size, mtime, file]
   end
 
-  def max_chars(files_with_details, index)
-    files_with_details.transpose[index].map { |file_with_detail| file_with_detail.to_s.strip.size }.max
+def margin(index)
+  if index.zero?
+    0
+  elsif [1, 2, 6].include?(index)
+    1
+  else
+    2
   end
+end
 
+def max_chars(files_with_details, index)
+  files_with_details.transpose[index].map { |file_with_detail| file_with_detail.to_s.strip.size }.max
+end
+
+def execute_ls_with_l_option(files, files_with_details)
+  blocks_total = files.map { |file| File.stat(file).blocks }.sum
   puts "total #{blocks_total}"
 
   files_with_details.each do |file_with_details|
@@ -108,21 +124,10 @@ if params[:l]
 
     puts file_with_details.join
   end
+end
+
+if params[:l]
+  execute_ls_with_l_option(files, files_with_details)
 else
-  indent = files.map(&:size).max + 1
-
-  NUMBER_OF_COLUMNS = 3
-  number_of_rows = (files.size.to_f / NUMBER_OF_COLUMNS).ceil
-
-  (0..number_of_rows - 1).each do |i|
-    files_for_row = []
-    index_of_file = i
-
-    while index_of_file <= files.size - 1
-      files_for_row << files[index_of_file].ljust(indent)
-      index_of_file += number_of_rows
-    end
-
-    puts files_for_row.join
-  end
+  execute_ls(files)
 end
