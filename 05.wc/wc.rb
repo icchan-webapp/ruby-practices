d21b0608f30124a22d584e97d7801109c8cae683
icchankun <ikkun.official@gmail.com>
2023-10-10 16:14:18 +0900

    頭揃えになっている箇所を修正。

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -51,9 +51,9 @@ def build_stat(params, lines, path: nil)
 end
 
 def build_total(params, path_stats)
-  total_lines =     path_stats.map { |stat| stat[:number_of_lines] }.sum if params[:number_of_lines]
-  total_words =     path_stats.map { |stat| stat[:number_of_words] }.sum if params[:number_of_words]
-  total_bytesizes = path_stats.map { |stat| stat[:bytesize] }.sum        if params[:bytesize]
+  total_lines = path_stats.map { |stat| stat[:number_of_lines] }.sum if params[:number_of_lines]
+  total_words = path_stats.map { |stat| stat[:number_of_words] }.sum if params[:number_of_words]
+  total_bytesizes = path_stats.map { |stat| stat[:bytesize] }.sum if params[:bytesize]
 
   [total_lines, total_words, total_bytesizes].compact
 end
