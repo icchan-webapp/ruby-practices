b4bc616de9a16edf9baf996a20b39fb360c055d6
icchankun <ikkun.official@gmail.com>
2024-04-18 18:34:48 +0900

    英語として自然にするため、変数名の単語の最後にsをつける。

--- 07.ls_object/file_display.rb
+++ 07.ls_object/file_display.rb
@@ -7,7 +7,7 @@ class FileDisplay
 
   def initialize
     @options = ARGV.getopts('alr').transform_keys(&:to_sym)
-    @files_stat = fetch_files_stat
+    @file_stats = fetch_file_stats
   end
 
   def format
@@ -16,7 +16,7 @@ class FileDisplay
 
   private
 
-  def fetch_files_stat
+  def fetch_file_stats
     flags = @options[:a] ? File::FNM_DOTMATCH : 0
     file_names = Dir.glob('*', flags)
     sorted_file_names = @options[:r] ? file_names.reverse : file_names
@@ -24,10 +24,10 @@ class FileDisplay
   end
 
   def long_format
-    blocks_total = @files_stat.map(&:blocks).sum
+    blocks_total = @file_stats.map(&:blocks).sum
     puts "total #{blocks_total}"
 
-    @files_stat.each { |file_stat| puts format_file_stat(file_stat) }
+    @file_stats.each { |file_stat| puts format_file_stat(file_stat) }
   end
 
   def format_file_stat(file_stat)
@@ -35,7 +35,7 @@ class FileDisplay
       stat_key = stat[0]
       stat_value = stat[1]
       margin = generate_margin(display_order_from_left)
-      max_chars = calc_max_chars(@files_stat, stat_key)
+      max_chars = calc_max_chars(@file_stats, stat_key)
       format_stat_value(display_order_from_left, stat_value, margin, max_chars)
     end.join
   end
@@ -51,8 +51,8 @@ class FileDisplay
     end
   end
 
-  def calc_max_chars(files_stat, stat_key)
-    files_stat.map { |file_stat| file_stat.build[stat_key].to_s.size }.max
+  def calc_max_chars(file_stats, stat_key)
+    file_stats.map { |file_stat| file_stat.build[stat_key].to_s.size }.max
   end
 
   def format_stat_value(display_order_from_left, stat_value, margin, max_chars)
@@ -62,7 +62,7 @@ class FileDisplay
   end
 
   def short_format
-    file_names = @files_stat.map(&:name)
+    file_names = @file_stats.map(&:name)
     width = file_names.map(&:size).max + 1
     number_of_rows = (file_names.size.to_f / NUMBER_OF_COLUMNS).ceil
     show_formatted_file_names(file_names, width, number_of_rows)
