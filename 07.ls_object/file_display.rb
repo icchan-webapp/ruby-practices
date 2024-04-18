4564fd6b91279652f13ad28c900fbe92c760508c
icchankun <ikkun.official@gmail.com>
2024-04-18 17:42:40 +0900

    外部に公開している属性がないが、attr_readerを指定していたため、削除する。

--- 07.ls_object/file_display.rb
+++ 07.ls_object/file_display.rb
@@ -3,8 +3,6 @@
 require 'optparse'
 
 class FileDisplay
-  attr_reader :options, :files_stat
-
   NUMBER_OF_COLUMNS = 3
 
   def initialize
@@ -13,23 +11,23 @@ class FileDisplay
   end
 
   def format
-    options[:l] ? long_format : short_format
+    @options[:l] ? long_format : short_format
   end
 
   private
 
   def fetch_files_stat
-    flags = options[:a] ? File::FNM_DOTMATCH : 0
+    flags = @options[:a] ? File::FNM_DOTMATCH : 0
     file_names = Dir.glob('*', flags)
-    options[:r] ? file_names.reverse! : file_names
+    @options[:r] ? file_names.reverse! : file_names
     file_names.map { |file_name| FileStat.new(file_name) }
   end
 
   def long_format
-    blocks_total = files_stat.map(&:blocks).sum
+    blocks_total = @files_stat.map(&:blocks).sum
     puts "total #{blocks_total}"
 
-    files_stat.each { |file_stat| puts format_file_stat(file_stat) }
+    @files_stat.each { |file_stat| puts format_file_stat(file_stat) }
   end
 
   def format_file_stat(file_stat)
@@ -37,7 +35,7 @@ class FileDisplay
       stat_key = stat[0]
       stat_value = stat[1]
       margin = generate_margin(display_order_from_left)
-      max_chars = calc_max_chars(files_stat, stat_key)
+      max_chars = calc_max_chars(@files_stat, stat_key)
       format_stat_value(display_order_from_left, stat_value, margin, max_chars)
     end.join
   end
@@ -64,7 +62,7 @@ class FileDisplay
   end
 
   def short_format
-    file_names = files_stat.map(&:name)
+    file_names = @files_stat.map(&:name)
     width = file_names.map(&:size).max + 1
     number_of_rows = (file_names.size.to_f / NUMBER_OF_COLUMNS).ceil
     show_formatted_file_names(file_names, width, number_of_rows)
