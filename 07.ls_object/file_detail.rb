4e407c01d33f7a5b8cbabec5cb1e1251a59a5aca
icchankun <ikkun.official@gmail.com>
2024-04-25 12:18:36 +0900

    mtimeもdelegateできるため、配列内に含める。

--- 07.ls_object/file_detail.rb
+++ 07.ls_object/file_detail.rb
@@ -8,7 +8,7 @@ class FileDetail
 
   attr_reader :name
 
-  delegate %i[blocks nlink size] => :@file_stat
+  delegate %i[blocks nlink size mtime] => :@file_stat
 
   def initialize(file_name)
     @file_stat = File.stat(file_name)
@@ -25,9 +25,5 @@ class FileDetail
 
   def group
     Etc.getgrgid(@file_stat.gid)
-  end
-
-  def mtime
-    @file_stat.mtime
   end
 end
